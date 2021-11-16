ed6446034cb0ffbd20c0944ee857858fa9f6b543
julio1703 <89707649+julio1703@users.noreply.github.com>
2021-11-16 16:29:44 +0200

    background js

---
+++ router.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+const handlers = require("./handlers");
+
+router.get("/", (req, res) => {
+  res.sendfile(path.join(__dirname, "public", "index.html"));
+});
+
+router.post("/log-in", (req, res) => {
+  res.redirect("/notes");
+});
+
+router.get("/notes", (req, res) => {
+  res.sendfile(path.join(__dirname, "public", "notes.html"));
+});
+router.use(express.static(path.join(__dirname, "public")));

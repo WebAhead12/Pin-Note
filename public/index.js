09e18c89a4df919ca463e5a9f4f3aeb528fd8448
deana17 <91081633+deana17@users.noreply.github.com>
2021-11-16 16:31:17 +0200

    login css

---
+++ public/index.js
@@ -0,0 +1,36 @@
+const form = document.querySelector(".signInContainer");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const par = document.getElementById("errPar");
+
+const errObj = {
+  username: "Please enter a username",
+  password: "Please enter your password",
+};
+
+const checkUser = function () {
+  if (!username.value) {
+    errPar.textContent = errObj.username;
+    return true;
+  }
+  if (!password.value) {
+    errPar.textContent = errObj.password;
+    return true;
+  }
+
+  return false;
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (checkUser()) return;
+  else {
+    errPar.textContent = "";
+  }
+
+  fetch("/log-in", {
+    method: "POST",
+    body: JSON.stringify({ username: username, password: password }),
+  });
+});

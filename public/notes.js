4d3348eccedd67a185cdc30931affa247e3a03b1
Zahi Mues <Zahi.moouess@gmail.com>
2021-11-17 10:38:01 +0200

    note adder

--- public/notes.js
+++ public/notes.js
@@ -1,8 +1,6 @@
-//Make the DIV element draggagle:
-const dragme = document.getElementById("note");
-const dragme1 = document.getElementById("note1");
-dragElement(dragme1);
-dragElement(dragme);
+
+const add = document.getElementById("Add-btn");
+
 
 function dragElement(elmnt) {
   var pos1 = 0,
@@ -27,8 +25,6 @@ function dragElement(elmnt) {
 
   function elementDrag(e) {
     e = e || window.event;
-    console.log(1);
-    console.log(e.target);
     e.preventDefault();
     // calculate the new cursor position:
     pos1 = pos3 - e.clientX;
@@ -55,4 +51,23 @@ function dragElement(elmnt) {
     document.onmouseup = null;
     document.onmousemove = null;
   }
+}
+//
+ add.addEventListener('click', addNote)
+// add a note and edit it
+function addNote(){
+  const body = document.querySelector("body")
+  const addanote = document.createElement('div')
+  const list = document.createElement("li")
+  const h2 = document.createElement('h2')
+  const paragraph = document.createElement('p')
+  addanote.classList.add('note')
+  h2.classList.add('title')
+  paragraph.classList.add('text')
+  list.classList.add('list')
+  body.appendChild(addanote)
+  addanote.appendChild(list)
+  list.appendChild(h2)
+  list.appendChild(paragraph)
+  dragElement(addanote);
 }
